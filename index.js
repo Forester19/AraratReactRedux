5012097a2f3eedaaba40f1551a48215e00864d84
Forester19 <vdvoreckij4@gmail.com>
2018-11-06 21:11:47 +0200

    'Redux'

--- index.js
+++ index.js
@@ -8,39 +8,40 @@ import {HeaderComponent, FooterComponent} from "./src/component/HeaderFooter";
 import Sign from './src/component/LogIn'
 import createBrowserHistory from 'history/createBrowserHistory';
 //import rootReducer from './src/reducers/holderReducers'
-
+const createState = () => ({
+    userInfo: {
+        login: "",
+        password: ""
+    }
+});
 
 const history = createBrowserHistory();
-//const store = createStore(rootReducer);
 
-/*
-class MainComponent extends React.Component {
-    render() {
-        return <BrowserRouter history={history}>
-                <HeaderComponent/>
-
-
-                <FooterComponent/>
-        </BrowserRouter>;
+let reducer = (state = createState(), action) => {
+    if (action.type === 'ADD_USER') {
+        return Object.assign({}, state, {userInfo: {login: action.payload.login, password: action.payload.password}});
     }
-}*/
+    return state;
+};
+const store = createStore(reducer);
+
+
 class MainComponent2 extends React.Component {
     render() {
-        return <BrowserRouter history={history}>
+        return <Provider store={store}>
+        <BrowserRouter history={history}>
             <div>
             <HeaderComponent/>
-                <Route path='/sign-in' render={function () {
-                    return <Sign name='signin'/>
-                }}/>
-                <Route path='/sign-up' render={function () {
-                    return <Sign name='signup'/>
-                }}/>
+                <Route path='/sign-in' render={()=> <Sign name='signin'/>}/>
+                <Route path='/sign-up' render={()=> <Sign name='signup'/>}/>
             <FooterComponent/>
             </div>
-        </BrowserRouter>;
+        </BrowserRouter>
+        </Provider>;
     }
 }
 
 
 
 ReactDOM.render(<MainComponent2 />, document.getElementById('root'));
+window.store = store;
--- src/component/LogIn.js
+++ src/component/LogIn.js
@@ -1,35 +1,66 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-class Sign extends React.Component{
-    render(){
+class Sign extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {login: '', password: ''};
+        this.setLogin = this.setLogin.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+        this.setCredentials = this.setCredentials.bind(this);
+        this.updateCredentials = this.updateCredentials.bind(this);
+    }
+    render() {
         return <div className={this.props.name}>
-        <form>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                       placeholder="Enter email"/>
+            <form onSubmit={function (event) {event.preventDefault();}} >
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                           placeholder="Enter email" onChange={this.setLogin}/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
                         else.
                     </small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-            </div>
-            <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={this.setPassword}
+                           placeholder="Password"/>
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
                     <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">{this.props.name}</button>
-        </form>
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={this.updateCredentials}>{this.props.name}</button>
+            </form>
         </div>
     }
+
+    setLogin(event) {
+        this.setState({login: event.target.value});
+    }
+
+    setPassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+    updateCredentials(){
+        this.props.dispatch(this.setCredentials());
+    }
+    setCredentials() {
+        return {
+            type: 'ADD_USER',
+            payload: {
+                login: this.state.login,
+                password: this.state.password,
+            }
+        };
+    }
 }
-/*function mapStateToProps(state){
+
+function mapStateToProps(state){
     return {
         login: state.userInfo.login,
         password: state.userInfo.password
     }
-}*/
-export default Sign;
+}
+export default connect(mapStateToProps)(Sign);
